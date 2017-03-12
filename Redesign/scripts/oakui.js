9d6c3210f67567c229d0d62998fd0964a41800be
Karim Abdel Hamid <karimah@aucegypt.edu>
2017-03-12 20:35:30 +0200

    Redesign v0.3

---
+++ Redesign/scripts/oakui.js
@@ -0,0 +1,50 @@
+function changeTheme() {
+    alert(1);    
+}
+
+$(document).ready(function() {
+    $("#themes").change(function() {
+        var themeID = $(this).val();
+        for (i = 0; i < 2; i++)
+            $("#theme"+i).prop('disabled', i!=themeID);
+    });
+    
+    $('.grabberX').on('mousedown', function(e){
+        var $element = $(this).parent();
+        var $element2 = $(this).parent().parent().find("section");
+        var $element3 = $(this).parent().parent().find("footer");
+        var width = $(this).parent().parent().width();
+        
+        $(document).on('mouseup', function(e){
+            $(document).off('mouseup').off('mousemove');
+        });
+
+        $(document).on('mousemove', function(me){
+            var mx = (me.pageX)*100.0/width;
+            mx = Math.min(90, Math.max(20, mx));
+
+            $element.css({width: ((100-mx)+"%")});
+            $element2.css({width: ((mx)+"%")});
+            $element3.css({width: ((mx)+"%")});
+        });
+    });
+    
+    $('.grabberY').on('mousedown', function(e){
+        var $element = $(this).parent();
+        var $element2 = $(this).parent().parent().find("footer");
+        var height = $(this).parent().parent().height();
+
+        var	pY = e.pageY-$(this).position().top+$element.position().top;
+        
+        $(document).on('mouseup', function(e){
+            $(document).off('mouseup').off('mousemove');
+        });
+        $(document).on('mousemove', function(me){
+            var my = (me.pageY - pY)*100.0/height;
+            my = Math.min(90, Math.max(15, my));
+
+            $element.css({height: ((my)+"%")});
+            $element2.css({height: ((100-my)+"%")});
+        });
+    });
+});
