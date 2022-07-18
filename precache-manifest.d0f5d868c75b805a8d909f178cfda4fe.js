c4c1d5ba81601e013d5decdcd07fe0bd1d62648f
Donn <me@donn.website>
2022-07-18 13:36:20 +0200

    Updates

---
+++ precache-manifest.d0f5d868c75b805a8d909f178cfda4fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e546ac709cee5ac21868b89e6d4ad61d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "33496a39c4a1f66bdaf1",
+    "url": "./static/js/2.7c05816f.chunk.js"
+  },
+  {
+    "revision": "e63617f8fbe5c0d54ed2b70fdb2ff677",
+    "url": "./static/js/2.7c05816f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46878ec50f96b40c01d0",
+    "url": "./static/js/main.c021d99f.chunk.js"
+  },
+  {
+    "revision": "0680ced136b6dcb92768",
+    "url": "./static/js/runtime-main.d70132fe.js"
+  }
+]);
